5623ca6e557204dd642ff32762a9316c136223ea
sagar_yenkure <yenkuresagar2104@gmail.com>
2025-06-28 14:03:45 +0530

    feat: update metadata description for clarity and improve font loading links

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Sachin Yenkure | Graphic Designer & Visual Artist',
   description:
-    'Explore the creative portfolio of Sachin Yenkure - a passionate graphic designer skilled in festival creatives, traditional art, Instagram posts, branding, and UX/UI visuals.',
+    'Explore Sachin Yenkure’s portfolio—graphic design, festival creatives, branding, UI/UX, and visual storytelling all in one place.',
   keywords:
     'Graphic Designer, UI/UX Designer, Festival Graphics, Instagram Post Designer, Branding Designer, Canva Designer, Figma, Social Media Graphics, Creative Portfolio, Visual Artist',
   metadataBase: new URL(process.env.APP_URL!),
@@ -42,7 +42,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: {
@@ -52,8 +51,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Space+Grotesk:wght@300;400;500;600;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Space+Grotesk:wght@300;400;500;600;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-inter antialiased">{children}</body>
     </html>
